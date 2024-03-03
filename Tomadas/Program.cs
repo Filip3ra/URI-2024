c50e3d79aa8c13ba4558574d55abf9042e458423
Filip3ra <filipimacielrj@gmail.com>
2024-03-03 15:00:01 -0300

    tomadas resolvido

--- Tomadas/Program.cs
+++ Tomadas/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+// Dividir a entrada em partes usando espaço como delimitador
+string[] valores = Console.ReadLine().Split(' ');
+
+// Converter os valores para os tipos desejados (por exemplo, int)
+int valor1 = int.Parse(valores[0]);
+int valor2 = int.Parse(valores[1]);
+int valor3 = int.Parse(valores[2]);
+int valor4 = int.Parse(valores[3]);
+
+int R = valor1 + valor2 + valor3 + valor4 - 3;
+
+Console.WriteLine(R);
