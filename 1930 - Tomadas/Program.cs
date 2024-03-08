4437f1cca13027eeb0ebf614a416d7e5f323e1f9
Filip3ra <filipimacielrj@gmail.com>
2024-03-08 09:39:42 -0300

    questao 2006 resolvida

---
+++ 1930 - Tomadas/Program.cs
@@ -0,0 +1,13 @@
+﻿
+// Dividir a entrada em partes usando espaço como delimitador
+string[] valores = Console.ReadLine().Split(' ');
+
+// Converter os valores para os tipos desejados (por exemplo, int)
+int valor1 = int.Parse(valores[0]);
+int valor2 = int.Parse(valores[1]);
+int valor3 = int.Parse(valores[2]);
+int valor4 = int.Parse(valores[3]);
+
+int R = valor1 + valor2 + valor3 + valor4 - 3;
+
+Console.WriteLine(R);
---
+++ 2006 - Identificando o chá/Program.cs
@@ -0,0 +1,15 @@
+﻿string s = Console.ReadLine();
+
+string valores = Console.ReadLine();
+
+int cont = 0;
+
+for (int i = 0; i < valores.Length; i++)
+{
+  if (valores[i] == s[0])
+    cont++;
+}
+
+Console.WriteLine(cont);
+
+
