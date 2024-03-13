fefbb1e2ec976822727f19c74a97eda0c0a3fd56
Filip3ra <filipimacielrj@gmail.com>
2024-03-13 12:33:59 -0300

    1924 resolvida

---
+++ 1924 - Vitória e a Indecisão/Program.cs
@@ -0,0 +1,22 @@
+﻿
+using System;
+
+class URI
+{
+
+  static void Main(string[] args)
+  {
+
+    // nao aceita o tratamento de caractere nulos, "?"
+    string s = Console.ReadLine();
+    int x;
+    int.TryParse(s, out x);
+    for (int i = 0; i < x; i++)
+    {
+      s = Console.ReadLine();
+    }
+    Console.WriteLine("Ciencia da Computacao");
+
+  }
+
+}
